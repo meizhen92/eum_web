9d5ff9e9af8952c7e951fab10ccd02fce050aade
ehch103 <ehch103@gmail.com>
2023-12-20 10:23:44 +0900

    default spring

---
+++ eum_web/src/main/java/com/eumWeb/main/EumWebApplication.java
@@ -0,0 +1,13 @@
+package com.eumWeb.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EumWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EumWebApplication.class, args);
+	}
+
+}
